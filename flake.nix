00e4592c41f47e7e5a3f117ad908b83a216574ea
Simon Renblad <srenblad@m-labs.hk>
2025-08-05 16:00:59 +0800

    add flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "a tool to view wikipedia pages from your terminal";
+
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+
+  outputs = { self, nixpkgs }:
+    let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      wik = pkgs.python3Packages.buildPythonPackage {
+        pname = "wik";
+        version = "0.1";
+        pyproject = true;
+        build-system = [pkgs.python3Packages.flit];
+        src = self;
+        propagatedBuildInputs = with pkgs.python3Packages; [ beautifulsoup4 requests ];
+      };
+    in {
+      packages.x86_64-linux.default = wik;
+    };
+}
